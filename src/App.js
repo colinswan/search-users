3b24804cf9bcf6480265cbfdf2deae6704541e07
Colin Swan <colinswan.cs@gmail.com>
2022-11-14 12:28:40 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // We access and generate state through the constructor method
+  constructor() {
+    // super() just calls the underlying constructor method of any other classes that we're extending from, so in this instance our App class is extending from Component, so we need to call super() to access the underlying constructor method of Component
+    super();
+
+    // We initialize our state object with an empty object with this key/value pair. key is "name" and value is 'Colin'
+
+    this.state = {
+      name: { firstName: "Colin", lastName: "Swan" },
+      company: "Google",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at
+            {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState({
+                name: { firstName: "Jake", lastName: "Hamilton" },
+                company: "Facebook",
+              });
+              console.log(this.state);
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
